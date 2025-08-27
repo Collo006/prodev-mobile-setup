4eb63c8dc6fc11cd4063f33c8496e4dcbe08f236
Collins Kipruto <collinskipruto036@gmail.com>
2025-08-27 18:43:47 +0300

    Submit

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View , StyleSheet } from "react-native";
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
   container:{
     backgroundColor: "#90caf9"
   },
@@ -29,10 +29,9 @@ const styles=StyleSheet.create({
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Entry Screen - Awesome.</Text>
       <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
 }
