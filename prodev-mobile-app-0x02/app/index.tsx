5efd216ff77099a5afd66dbade80407ac9dbf79b
Collins Kipruto <collinskipruto036@gmail.com>
2025-08-28 10:47:15 +0300

    submit

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,6 +1,15 @@
-import { Text, View, StyleSheet, ImageBackground} from "react-native";
+import { Text, View, ImageBackground, StyleSheet, Dimensions, Image} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const styles = StyleSheet.create({
+background:{
+  height: Dimensions.get("window").height, //covers the entire screen
+  width: Dimensions.get("window").width //covers the entire screen
+},
+companyLogo:{
+  borderColor: "#000" 
+}
+})
 
 export default function Index() {
   return (
@@ -11,11 +20,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text>BOOKS</Text>
     <SafeAreaProvider>
       <SafeAreaView style={{flex:1}}>
-        <ImageBackground source={require("@/assets/images/background-image.png")}style={styles.background}  resizeMode="cover">
+        <ImageBackground source={require("@/assets/images/background-image.png")} style={styles.background} resizeMode="cover">
         {/*Other Components go here */}
-
+       <View style={styles.companyLogo}>
+        <Image source={require("@/assets/images/Logo.png")}></Image>
+       </View>
         </ImageBackground>
       </SafeAreaView>
     </SafeAreaProvider>
